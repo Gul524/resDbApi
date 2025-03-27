98fb97607f5e5605b1fe9410a67e0fc03218b684
Gul524 <gullsuleman524@gmail.com>
2025-03-28 03:34:19 +0500

    Updated .gitignore to exclude Constraints.java

--- src/main/java/com/asadevelopers/resDbApi/Constraints.java
+++
@@ -1,6 +0,0 @@
-package com.asadevelopers.resDbApi;
-
-public class Constraints {
-    static public String apiUsername = "branchApi";
-    static public String apiPassword = "branchApiPassword123";
-}
